fd19807c7707877b3b45446cb9523027de43c128
295238641@qq.com <295238641@qq.com>
2021-12-12 17:19:53 +0800

    pass debug

--- agent/index.ts
+++ agent/index.ts
@@ -5,7 +5,30 @@ import {dumper} from "./dumper";
 setImmediate(main)
 
 function main() {
-    dumper.start();
-    // log("hookStart");
+    // dumper.start();
+    log("hookStart");
+    waitHookDlopen()
+
+    function waitHookDlopen() {
+
+
+
+        const dlopen_old = Module.findExportByName(null, "dlopen")
+        const dlopen_new = Module.findExportByName(null, "android_dlopen_ext")
+        if (dlopen_old != null) {
+            Interceptor.attach(dlopen_old, {
+                onEnter: function (args) {
+                    var l_soName = args[0].readCString()
+                    log("soName:" + l_soName)
+                },
+                onLeave: function (retval) {
+                    if (this.hook) {
+                        // send("onLeave")
+                    }
+                }
+            })
+        }
+
+    }
 }
 
